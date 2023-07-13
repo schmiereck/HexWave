73d94285d170b835c428af88303eef88c1f832e4
Thomas Schmiereck <thomas.schmiereck@arvato.com>
2023-07-13 21:57:13 +0200

    AccelerationTest2.

--- src/test/java/de/schmiereck/hexWave/Acceleration2Test.java
+++ src/test/java/de/schmiereck/hexWave/Acceleration2Test.java
@@ -1,13 +1,10 @@
 package de.schmiereck.hexWave;
 
-import java.util.function.Predicate;
-
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class AccelerationTest {
+public class Acceleration2Test {
     @Test
     public void acceleration() {
         int xPos = 10;
@@ -20,10 +17,10 @@ public class AccelerationTest {
         final int MaxMove = 60;
         final int MaxVelocity = MaxMove + MaxMove;
 
-        final int xGravitation = -10;
+        //final int xGravitation = -10;
         //final int xGravitation = 10;
 
-        //final int xGravitation = -15;
+        final int xGravitation = -15;
         //final int xGravitation = 15;
 
         //final int xGravitation = -19;
@@ -32,11 +29,14 @@ public class AccelerationTest {
         for (int cnt = 0; cnt < 300; cnt++) {
             xOutAcceleration = xGravitation;
 
-            //xAMove += calcSaveFract(xOutAcceleration, xVelocity, MaxMove);
-            xAMove += xOutAcceleration;
+            xAMove += (xOutAcceleration);
 
-            final int moveDir = calcMoveDir(xAMove, xVMove, MaxMove);
-            //final int moveDir = calcMoveDir2(xAMove, xVMove, MaxMove);
+            //xVMove += (xVelocity + xOutAcceleration);
+            xVMove += calcVMove(xOutAcceleration, xVelocity + xAMove, MaxVelocity);
+            //xVMove += (xVelocity + xAMove);
+            //xVMove = calcVMove(xAMove, xVMove, xVelocity, MaxVelocity);
+
+            final int moveDir = calcMoveDir(xVMove, MaxMove);
             // Will Move?
             if (moveDir != 0) {
                 // Blocked?
@@ -45,22 +45,19 @@ public class AccelerationTest {
                     //xVMove -= ((MaxMove + MaxMove) * moveDir);
                     xVelocity = -xVelocity;
                     //xVelocity = (calcNewVelocity(xVelocity, xAMove, MaxVelocity));
-                    xVMove += xVelocity - ((MaxMove + MaxMove) * moveDir); // Korrektur-Hack!!!! //xVMove += 30;  für moveDir = -1
-                    //xVMove += xVelocity; // Korrektur-Hack!!!! //xVMove += 30;  für moveDir = -1
-                    //xVMove = -xVMove;
-                    //xVMove -= (MaxMove + MaxMove) * moveDir;
-                    //xVMove += 10;
-                    xAMove -= xOutAcceleration;
-                    //xAMove = -xAMove;
-                    //xAMove = 0;
+                    //xVMove += xVelocity - ((MaxMove + MaxMove) * moveDir); // Korrektur-Hack!!!! //xVMove += 30;  für moveDir = -1
+                    xVMove = -xVMove;
+                    xVMove -= (MaxMove + MaxMove) * moveDir;
+                    xAMove = 0;
+                    //xAMove -= xOutAcceleration;
                     System.out.printf("xPos:%2d, xAMove:%4d, xVMove:%4d, xVelocity:%4d\t <- HIT-AFTER  ----\n", xPos, xAMove, xVMove, xVelocity);
                 } else {
                     // Move:
                     System.out.printf("xPos:%2d, xAMove:%4d, xVMove:%4d, xVelocity:%4d\t <- MOVE-BEVOR\n", xPos, xAMove, xVMove, xVelocity);
 
-                    //xVelocity += xAMove;
-                    xVelocity = calcNewVelocity(xVelocity, xAMove, MaxVelocity);
-                    xVMove = calcVMove(xAMove, xVMove, xVelocity, MaxVelocity);
+                    xVelocity += xAMove;
+                    //xVelocity = calcNewVelocity(xVelocity, xVMove, MaxVelocity);
+                    //xVMove = calcVMove(xVMove, xVelocity, MaxVelocity);
                     xAMove = 0;
 
                     xVMove -= (MaxMove + MaxMove) * moveDir;
@@ -71,18 +68,15 @@ public class AccelerationTest {
                 }
             } else {
                 System.out.printf("xPos:%2d, xAMove:%4d, xVMove:%4d, xVelocity:%4d\t <- STAY-BEVORE\n", xPos, xAMove, xVMove, xVelocity);
-                xVMove = calcVMove(xAMove, xVMove, xVelocity, MaxVelocity);
                 System.out.printf("xPos:%2d, xAMove:%4d, xVMove:%4d, xVelocity:%4d\t <- STAY-AFTER\n", xPos, xAMove, xVMove, xVelocity);
             }
-
-            //xVMove = calcVMove(xAMove, xVMove, xVelocity, MaxVelocity);
         }
     }
 
-    private static int calcVMove(int xAMove, int xVMove, int xVelocity, int MaxVelocity) {
+    private static int calcVMove(int xVMove, int xVelocity, int MaxVelocity) {
         //xVMove += xVelocity + xAMove;
         //xVMove += xVelocity + xOutAcceleration;
-        xVMove += xVelocity + calcNewAcceleration(xVelocity, xAMove, MaxVelocity);
+        xVMove += xVelocity + calcNewAcceleration(xVelocity, xVMove, MaxVelocity);
         //xVMove += xVelocity + calcNewAcceleration(xVelocity, xOutAcceleration, MaxVelocity);
         return xVMove;
     }
@@ -109,21 +103,21 @@ public class AccelerationTest {
         }
     }
 
-    private static int calcNewAcceleration(final int xVelocity, final int xAMove, final int MaxVelocity) {
+    private static int calcNewAcceleration(final int xVelocity, final int xVMove, final int MaxVelocity) {
         // m(v) = m0 / (sqrt(1 - (v² / c²)))
         // m(v) = m0 * (c / (c - v))
         //int nv = xVelocity + xAMove;
-        //int nm = Math.ceilDiv((MaxVelocity - nv) * xAMove, MaxVelocity);
+        //int nm = Math.ceilDiv((MaxVelocity - nv) * xVMove, MaxVelocity);
 
-        //int nm = Math.ceilDiv(((MaxVelocity - Math.abs(xVelocity)) * xAMove), MaxVelocity);
-        //int nm = (int)Math.floorDiv((long)((MaxVelocity - Math.abs(xVelocity)) * xAMove), (long)MaxVelocity);
-        int nm = ((MaxVelocity - Math.abs(xVelocity)) * xAMove) / MaxVelocity;
+        //int nm = Math.ceilDiv(((MaxVelocity - Math.abs(xVelocity)) * xVMove), MaxVelocity);
+        //int nm = (int)Math.floorDiv((long)((MaxVelocity - Math.abs(xVelocity)) * xVMove), (long)MaxVelocity);
+        int nm = ((MaxVelocity - Math.abs(xVelocity)) * xVMove) / MaxVelocity;
 
-        return nm;
+        return nm;// + (xVMove == 0 ? 0 : (xVMove > 0 ? 1 : -1));
     }
 
-    private static int calcNewVelocity(final int xVelocity, final int xAMove, final int MaxVelocity) {
-        int nm = calcNewAcceleration(xVelocity, xAMove, MaxVelocity);
+    private static int calcNewVelocity(final int xVelocity, final int xVMove, final int MaxVelocity) {
+        int nm = calcNewAcceleration(xVelocity, xVMove, MaxVelocity);
 
         return calcMinMax(xVelocity + nm, MaxVelocity);
     }
@@ -172,24 +166,24 @@ public class AccelerationTest {
         final int MaxVelocity = MaxMove + MaxMove;
         {
             int xVMove = 0;
-            MatcherAssert.assertThat(calcMoveDir(0, xVMove, MaxMove), Matchers.equalTo(0));
+            MatcherAssert.assertThat(calcMoveDir(xVMove, MaxMove), Matchers.equalTo(0));
             xVMove += MaxMove;
-            MatcherAssert.assertThat(calcMoveDir(0, xVMove, MaxMove), Matchers.equalTo(1));
+            MatcherAssert.assertThat(calcMoveDir(xVMove, MaxMove), Matchers.equalTo(1));
         }
         {
             int xVMove = -(MaxMove - 1);
-            MatcherAssert.assertThat(calcMoveDir(0, xVMove, MaxMove), Matchers.equalTo(0));
+            MatcherAssert.assertThat(calcMoveDir(xVMove, MaxMove), Matchers.equalTo(0));
             xVMove += MaxVelocity;
-            MatcherAssert.assertThat(calcMoveDir(0, xVMove, MaxMove), Matchers.equalTo(1));
+            MatcherAssert.assertThat(calcMoveDir(xVMove, MaxMove), Matchers.equalTo(1));
         }
     }
 
-    private static int calcMoveDir(final int xAMove, final int xVMove, final int MaxMove) {
+    private static int calcMoveDir(final int xVMove, final int MaxMove) {
         final int moveDir;
-        if ((xAMove + xVMove) >= MaxMove) {
+        if ((xVMove) >= MaxMove) {
             moveDir = 1;
         } else {
-            if ((xAMove + xVMove) <= -MaxMove) {
+            if ((xVMove) <= -MaxMove) {
                 moveDir = -1;
             } else {
                 moveDir = 0;
